1a3778ee219d35b7c59d830ea61b7427da0d7357
Josh <josh.williams@ttu.edu>
2020-01-30 16:58:35 -0600

    work in progress, more comments added

--- P01_LinearRegression_Linear.m
+++ P01_LinearRegression_Linear.m
@@ -1,7 +1,7 @@
 close all; clear; clc;
 
 load carbig.mat
-scatter(Weight, Horsepower,'x');
+%scatter(Weight, Horsepower,'x');
 
 
 
@@ -9,10 +9,24 @@ scatter(Weight, Horsepower,'x');
 
 
 %% Closed form
-%y = predicted values
-%solution = W_1 * n + W_0
+%	(training data)
+%x = input x values (matrix)
+%t = input y values (matrix)
+%n = num datapoints (scalar)
 
+%y = predicted values
+
+%X = design matrix (2 columns, n rows)
+%
 %W = ( (X')*(X) )^(-1) * (X') * t;
+%solution: y = W_1 * X + W_0
+
+%first, to fix NaN problems in dataset, calculate the mean of the dataset
+%and replace NaN with the mean
+horsepower_sum = nansum(Horsepower)
+Horsepower(isnan(Horsepower)) = 
+
+
 
 %% Gradient descent
 %gradient = 2*(W')*(X')*X - 2*(t')*X;
