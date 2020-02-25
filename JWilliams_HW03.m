9576ec274d1c02f294a2bbe290275ad36a984b5f
Josh <josh.williams@ttu.edu>
2020-02-25 14:32:23 -0600

    Update JWilliams_HW03.m

--- JWilliams_HW03.m
+++ JWilliams_HW03.m
@@ -5,10 +5,16 @@
 close all; clear; clc;
 
 seed = 0;
+M = 2; %--??
+
 %% Generate Dataset
 rng(seed);
+
 L = 100;
 N = 25;
-X = rand(N,1);
+X = rand(N,1);		%uniform distribution U(0,1)
+
 epsilon = normrnd(0,0.3,[1 N]);
 t(:) = sin(2.*pi.*X(:)) + epsilon(:);
+
+lambda = 0:0.0001:7;
