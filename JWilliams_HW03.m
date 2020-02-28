37cc56b8b222fc4494091fdb705023c221d90ed2
Josh <josh.williams@ttu.edu>
2020-02-28 12:07:21 -0600

    Update JWilliams_HW03.m

--- JWilliams_HW03.m
+++ JWilliams_HW03.m
@@ -5,16 +5,29 @@
 close all; clear; clc;
 
 seed = 0;
-M = 2; %--??
 
 %% Generate Dataset
 rng(seed);
 
 L = 100;
 N = 25;
-X = rand(N,1);		%uniform distribution U(0,1)
+X = rand(1,N);		%uniform distribution U(0,1)
 
 epsilon = normrnd(0,0.3,[1 N]);
 t(:) = sin(2.*pi.*X(:)) + epsilon(:);
 
-lambda = 0:0.0001:7;
+lambda = 0;
+
+%% create basis function
+
+mu = 0.5;
+s = 0.1;
+
+phi = [ones(1,N); exp(-(X-mu).^2/(2*s.^2))]';
+
+%compute closed-form analytical solution
+W = (phi'*phi + lambda*eye(2))\phi'*t';
+
+y = W'.*phi';
+
+scatter(X,y);
