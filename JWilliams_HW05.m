40fe46d30b8ee2db4f0d316a1c05053e48169aa1
Josh <josh.williams@ttu.edu>
2020-04-26 00:42:17 -0500

    got part a working

--- JWilliams_HW05.m
+++ JWilliams_HW05.m
@@ -13,35 +13,37 @@ epochs = 3000;
 num_inputs = 2;
 num_hidden_units = 2;
 num_hidden_layers = 1;
-num_outputs = 1;    %implement later with two classes?
+num_outputs = 1;    %implement later with two output classes?
 
 %% ml stuff
 % initialize neural network object
 network = neuralnetwork(num_inputs, num_hidden_units, num_outputs);
 
 % train
+figure
+hold on;
+set(gca,'yscale','log');
+xlim([0 epochs]);
+xlabel('Epochs');
+ylabel('Error');
 
-% figure
-% hold on;
+divisor = 2; %for plotting
 for i = 1:epochs
     network = nn_train(network, train_input, train_t, rho);
+    
+    % plot loss function at specific epochs
+    if( mod(i,floor(divisor)) == 0 )
+        tot_error = sum(0.5.*(network.output(:)-train_t(:)).^2);
+        plot(i, tot_error, '.r' );
 
-%     disp( mean(abs(train_t - network.output)) );
-     
-%      plot(i, -sum(train_t.*log10(network.output)), '.r' );
-%       
-%      set(gca,'yscale','log');
-%      xlim([0 epochs]);
-%      pause(0);
-     
-%     clc;
-%     disp(epochs);
-%     disp(network.input);
-%     disp(train_t);
-%     disp(network.output);
+        divisor = divisor.*1.005; %plot points less frequently as time goes on
+        pause(0);
+    end
+    
 end
 
 %% output results
+
 fprintf('results:\n\n');
 fprintf('epochs: %d\n\n', epochs);
 fprintf('  input \tnetwork output\n');
@@ -49,47 +51,58 @@ fprintf('  input \tnetwork output\n');
 for index=1:4
     fprintf('%3d\t', train_input(index,:));
     network = feedforward(train_input(index,:), network);
-    disp(network.output);
+    disp(network.output(index));
 end
+
+
+
 
 
 %% functions
 
 %train the nn using gradient descent
 function network = nn_train(network, input, t, rho)
+    
+    % calculate output value(s) from each unit
     network = feedforward(input, network);
     
-    network.error_out = t - network.output;
-    %network.error_out = -sum(t.*log10(network.output));
     
-    d_predicted_output = network.error_out.*(network.output.*(1-network.output));
+    % backprop (calculate gradients and adjust every W)
     
-    error_h = d_predicted_output*(network.W{2}');
-    d_hidden = error_h.*(network.h_out.*(1-network.h_out));
+    % hidden-to-output layer (L) %
+    lowercase_delta_output = network.output-t;
+    gradient_out = network.h_out'*lowercase_delta_output;
     
-    % update equations
-    network.W{2} = network.W{2} + ((network.h_out')*d_predicted_output).*rho;
-    network.bias{2} = network.bias{2} + sum(d_predicted_output).*rho;
+    %update equations
+    network.W{2} = network.W{2} - rho.*gradient_out;
+    network.bias{2} = network.bias{2} - rho.*lowercase_delta_output;
     
-    network.W{1} = network.W{1} + ((network.input')*d_hidden).*rho;
-    network.bias{1} = network.bias{1} + sum(d_hidden).*rho;    
+    
+    % input-to-hidden layer (L-1) %
+    d_sigmoid = network.output.*(1-network.output); %h'(x) = h(x)(1-h(x))
+    
+    lowercase_delta_hidden = d_sigmoid.*lowercase_delta_output*sum(network.W{2});
+    gradient_hidden = network.input'*lowercase_delta_hidden;
+    
+    %update equations
+    network.W{1} = network.W{1} - rho.*gradient_hidden;
+    network.bias{1} = network.bias{1} - rho.*lowercase_delta_hidden;
 end
 
 
-%use pre-initialized neural network architecture, generates network output(s) (or "guess)
+%use pre-initialized neural network architecture, generates network output(s)
 function network = feedforward(inputs, network)
     
     %column vector (length = num inputs)
     network.input = inputs;
 
     %column vector (length = num hidden units)
-    network.h_out = sigmoid((network.input*network.W{1}) + network.bias{1});   %({1} is input-to-hidden layer)
+    network.h_out = sigmoid((network.input*network.W{1}) + network.bias{1});   %{1} is input-to-hidden layer
     
     %column vector (length = num outputs)
-    network.output = sigmoid((network.h_out*network.W{2}) + network.bias{2});
+    network.output = sigmoid((network.h_out*network.W{2}) + network.bias{2});  %{2} is hidden-to-output layer
 
 end
-
 
 %initialize neural network object
 function network_obj = neuralnetwork(num_inputs, num_hidden_units, num_outputs)
@@ -109,11 +122,11 @@ function network_obj = neuralnetwork(num_inputs, num_hidden_units, num_outputs)
     network_obj.bias{2} = rand(1, num_outputs);
     
     network_obj.h_out = [];         %output of hidden layer
-    network_obj.h_out_error = [];   %error associated with output of hidden layer
+    %network_obj.h_out_error = [];   %error associated with output of hidden layer
     
     % output
     network_obj.output = [];        %output of nn
-    network_obj.error_out = [];  %error associated with output of nn
+    %network_obj.error_out = [];     %error associated with output of nn
     
 end
 
